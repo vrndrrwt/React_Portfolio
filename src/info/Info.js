3efce225e62dc394c2d76616e07248c3f2dcb06c
vrndrrwt <virendrasinghrawat2812@gmail.com>
2025-02-16 10:36:51 +0530

    Update

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,5 +1,5 @@
 import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
+import mock1 from "../img/mock.png"
 import mock2 from "../img/mock2.png"
 import mock3 from "../img/mock3.png"
 import mock4 from "../img/mock4.png"
@@ -17,33 +17,19 @@ export const info = {
     baseColor: colors[0],
     miniBio: [
         {
-            emoji: '☕',
-            text: 'fueled by coffee'
-        },
-        {
             emoji: '🌎',
-            text: 'based in the India'
+            text: 'India'
         },
         {
             emoji: "💼",
-            text: "Student at BTU"
+            text: "BTU"
         },
         {
             emoji: "📧",
-            text: "vrndrrwtofficial@gmail.com"
+            text: "vrndrrwtt@gmail.com"
         }
     ],
     socials: [
-        {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
-        {
-            link: "https://instagram.com/virendrasinghrawatonline",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
         {
             link: "https://github.com/vrndrrwt",
             icon: "fa fa-github",
@@ -53,36 +39,27 @@ export const info = {
             link: "https://linkedin.com/in/vrndrrwt",
             icon: "fa fa-linkedin",
             label: 'linkedin'
-        },
-        {
-            link: "https://twitter.com/vrndrrwt",
-            icon: "fa fa-twitter",
-            label: 'twitter'
         }
 
     ],
     bio: "Hello! I'm Virendra Singh Rawat. I'm a aspiring a Software Engineer. You should hire me!",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'Django'],
-            exposedTo: ['nodejs', 'python']
+            proficientWith: ['Javascript', 'React', 'Git', 'Github', 'Bootstrap', 'Html5', 'CSS3', 'Django'],
+            exposedTo: ['Nodejs', 'Python']
         }
     ,
     hobbies: [
         {
-            label: 'reading',
+            label: 'Reading',
             emoji: '📖'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
-        },
-        {
-            label: 'movies',
+            label: 'Movies',
             emoji: '🎥'
         },
         {
-            label: 'cooking',
+            label: 'Cooking',
             emoji: '🌶'
         }
 // Same as above, change the emojis to match / relate to your hobbies or interests.
@@ -93,7 +70,7 @@ export const info = {
             title: "Project 1",
             live: "", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
             source: "https://github.com/vrndrrwt", // this should be a link to the **repository** of the project, where the code is hosted.
-            image: mock1
+            image: mock
         },
         {
             title: "Project 2",
